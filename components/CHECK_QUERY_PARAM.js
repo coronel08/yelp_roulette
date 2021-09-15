4b0ff24996ae4864b95a2d8025f543f7406e6265
Fidel C <coronel08@aol.com>
2021-09-14 17:38:36 -0700

    removed check query param, causing problems

--- components/CHECK_QUERY_PARAM.js
+++ components/CHECK_QUERY_PARAM.js
@@ -1,12 +1,12 @@
-import {useEffect} from 'react'
-import {useRouter} from 'next/router'
+// import {useEffect} from 'react'
+// import {useRouter} from 'next/router'
 
-export default function checkQueryParam(props) {
-    useEffect(() => {
-        console.log(props)
-        const router = useRouter();
-        if (props.query.term == "" || props.query.location == "") {
-            router.push("/");
-        }
-    }, [])
-}
+// export default function checkQueryParam(props) {
+//     useEffect(() => {
+//         console.log(props)
+//         const router = useRouter();
+//         if (props.query.term == "" || props.query.location == "") {
+//             router.push("/");
+//         }
+//     }, [])
+// }
